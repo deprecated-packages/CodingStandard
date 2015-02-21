09d09f45cb2013b62be54fa7a2fcaec085e8c5ad
Tomas Votruba <tomas.vot@gmail.com>
2015-02-21 15:36:47 +0100

    tests: ControlSignatureSniff test added

---
+++ tests/Sniffs/ControlStructures/ControlSignature/ControlSignatureSniffTest.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace Zenify\CodingStandard\Tests\Sniffs\ControlStructures\ControlSignature;
+
+use PHPUnit_Framework_TestCase;
+use Zenify\CodingStandard\Tests\CodeSnifferRunner;
+
+
+class ControlSignatureSniffTest extends PHPUnit_Framework_TestCase
+{
+
+	/**
+	 * @var CodeSnifferRunner
+	 */
+	private $codeSnifferRunner;
+
+
+	protected function setUp()
+	{
+		$this->codeSnifferRunner = new CodeSnifferRunner('ZenifyCodingStandard.ControlStructures.ControlSignature');
+	}
+
+
+	public function testDetection()
+	{
+		$this->assertSame(9, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/wrong.php'));
+        $this->assertSame(0, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct.php'));
+        $this->assertSame(0, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct2.php'));
+    }
+
+}
