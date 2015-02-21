ad6b0ffda453eea079b721795a148e192ce9aed9
Tomas Votruba <tomas.vot@gmail.com>
2015-02-21 15:15:14 +0100

    cs fixes

--- src/ZenifyCodingStandard/Sniffs/Commenting/VarPropertyCommentSniff.php
+++ src/ZenifyCodingStandard/Sniffs/Commenting/VarPropertyCommentSniff.php
@@ -18,33 +18,33 @@ use PHP_CodeSniffer_Standards_AbstractVariableSniff;
 class VarPropertyCommentSniff extends PHP_CodeSniffer_Standards_AbstractVariableSniff
 {
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function processMemberVar(PHP_CodeSniffer_File $file, $position)
-    {
-        $commentToken = [T_COMMENT, T_DOC_COMMENT_CLOSE_TAG];
-        $commentEnd = $file->findPrevious($commentToken, $position);
-        if ($commentEnd === FALSE) {
-            $file->addError('Property should have docblock comment', $position);
-            return;
-        }
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function processMemberVar(PHP_CodeSniffer_File $file, $position)
+	{
+		$commentToken = [T_COMMENT, T_DOC_COMMENT_CLOSE_TAG];
+		$commentEnd = $file->findPrevious($commentToken, $position);
+		if ($commentEnd === FALSE) {
+			$file->addError('Property should have docblock comment', $position);
+			return;
+		}
+	}
 
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function processVariable(PHP_CodeSniffer_File $file, $position)
-    {
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function processVariable(PHP_CodeSniffer_File $file, $position)
+	{
+	}
 
 
-    /**
-     * {@inheritdoc}
-     */
-    protected function processVariableInString(PHP_CodeSniffer_File $file, $position)
-    {
-    }
+	/**
+	 * {@inheritdoc}
+	 */
+	protected function processVariableInString(PHP_CodeSniffer_File $file, $position)
+	{
+	}
 
 }
