5f28e1e5236e69d019c09ad932d404bb0e7f3867
Tomas Votruba <tomas.vot@gmail.com>
2015-10-07 14:20:25 +0200

    tests: added forgotten test case

--- tests/Sniffs/Commenting/MethodComment/MethodCommentSniffTest.php
+++ tests/Sniffs/Commenting/MethodComment/MethodCommentSniffTest.php
@@ -29,6 +29,7 @@ class MethodCommentSniffTest extends PHPUnit_Framework_TestCase
 		$this->assertSame(1, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/wrong.php'));
 		$this->assertSame(0, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct.php'));
 		$this->assertSame(0, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct2.php'));
+		$this->assertSame(0, $this->codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct3.php'));
 	}
 
 }
