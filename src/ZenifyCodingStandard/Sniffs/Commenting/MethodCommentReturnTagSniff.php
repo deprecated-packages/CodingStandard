eb076bbeb15e5529a57696b30b7b578b48d6279f
Tomas Votruba <tomas.vot@gmail.com>
2015-10-27 19:13:19 +0100

    use own MethodCommentReturnTag sniff, ref #30

---
+++ src/ZenifyCodingStandard/Sniffs/Commenting/MethodCommentReturnTagSniff.php
@@ -0,0 +1,86 @@
+<?php
+
+/**
+ * This file is part of Zenify
+ * Copyright (c) 2012 Tomas Votruba (http://tomasvotruba.cz)
+ */
+
+namespace ZenifyCodingStandard\Sniffs\Commenting;
+
+use PHP_CodeSniffer_File;
+use PHP_CodeSniffer_Sniff;
+use ZenifyCodingStandard\Helper\Commenting\MethodDocBlock;
+
+
+/**
+ * Rules:
+ * - Getters should have @return tag (except {@inheritdoc}).
+ */
+final class MethodCommentReturnTagSniff implements PHP_CodeSniffer_Sniff
+{
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function register()
+	{
+		return [T_FUNCTION];
+	}
+
+
+	/**
+	 * @var string[]
+	 */
+	private $getterMethodPrefixes = ['get', 'is', 'has', 'will', 'should'];
+
+
+	/**
+	 * {@inheritdoc}
+	 */
+	public function process(PHP_CodeSniffer_File $file, $position)
+	{
+		$methodName = $file->getDeclarationName($position);
+		$isGetterMethod = $this->guessIsGetterMethod($methodName);
+		if ($isGetterMethod === FALSE) {
+			return;
+		}
+
+		if (MethodDocBlock::hasMethodDocBlock($file, $position) === FALSE) {
+			$file->addError('Getters should have docblock.', $position);
+			return;
+		}
+
+		$commentString = MethodDocBlock::getMethodDocBlock($file, $position);
+		if (strpos($commentString, '@return') !== FALSE) {
+			return;
+		}
+
+		$file->addError('Getters should have @return tag (except {@inheritdoc}).', $position);
+	}
+
+
+	/**
+	 * @param string $methodName
+	 * @return bool
+	 */
+	private function guessIsGetterMethod($methodName)
+	{
+		foreach ($this->getterMethodPrefixes as $getterMethodPrefix) {
+			if (strpos($methodName, $getterMethodPrefix) === 0) {
+				if (strlen($methodName) === strlen($getterMethodPrefix)) {
+					return TRUE;
+				}
+
+				$endPosition = strlen($getterMethodPrefix);
+				$firstLetterAfterGetterPrefix = $methodName[$endPosition];
+
+				if (ctype_upper($firstLetterAfterGetterPrefix)) {
+					return TRUE;
+				}
+			}
+		}
+
+		return FALSE;
+	}
+
+}
---
+++ tests/Sniffs/Commenting/MethodCommentReturnTag/FunctionCommentSniffTest.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Zenify\CodingStandard\Tests\Sniffs\Commenting\MethodCommentReturnTag;
+
+use PHPUnit_Framework_TestCase;
+use Zenify\CodingStandard\Tests\CodeSnifferRunner;
+
+
+/**
+ * @covers ZenifyCodingStandard\Sniffs\Commenting\MethodCommentReturnTagSniff
+ */
+final class FunctionCommentSniffTest extends PHPUnit_Framework_TestCase
+{
+
+	public function testDetection()
+	{
+		$codeSnifferRunner = new CodeSnifferRunner('ZenifyCodingStandard.Commenting.MethodCommentReturnTag');
+
+		$this->assertSame(1, $codeSnifferRunner->getErrorCountInFile(__DIR__ . '/wrong.php'));
+		$this->assertSame(1, $codeSnifferRunner->getErrorCountInFile(__DIR__ . '/wrong2.php'));
+		$this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct.php'));
+		$this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct2.php'));
+		$this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct3.php'));
+		$this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct4.php'));
+		$this->assertSame(0, $codeSnifferRunner->getErrorCountInFile(__DIR__ . '/correct5.php'));
+	}
+
+}
